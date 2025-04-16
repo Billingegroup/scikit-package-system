5022f9129d9469908334498dd8060b88db8abb1a
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-04-16 17:52:07 -0400

    chore: apply pre-commit automatically

--- {{ cookiecutter.folder_name }}/tests/test_calculator.py
+++ {{ cookiecutter.folder_name }}/tests/test_calculator.py
@@ -1,5 +1,6 @@
 import calculator as calc
 
+
 def test_dot_product_2D():
     """Test the dot product function in 2D."""
     a = [1, 2]
@@ -7,10 +8,10 @@ def test_dot_product_2D():
     expected = 11
     assert calc.dot_product(a, b) == expected
 
+
 def test_dot_product_3D():
     """Test the dot product function in 3D."""
     a = [1, 2, 3]
     b = [4, 5, 6]
     expected = 32
     assert calc.dot_product(a, b) == expected
-
